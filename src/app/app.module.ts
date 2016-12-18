3dc0f8f9784dd2f00a97c54ef53743215062021a
jesse <Teacher@Teachers-MacBook-Pro.local>
2016-12-18 15:02:59 +1300

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyBLF6Pyh4BJPYhQlhO6ZiqBIdQYROec0AI",
+    authDomain: "tracking-app-77db8.firebaseapp.com",
+    databaseURL: "https://tracking-app-77db8.firebaseio.com",
+    storageBucket: "tracking-app-77db8.appspot.com",
+    messagingSenderId: "285724076007"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +29,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig) 
   ],
   providers: [],
   bootstrap: [AppComponent]
