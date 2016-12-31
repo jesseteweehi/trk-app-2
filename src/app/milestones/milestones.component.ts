c1b12d72bbca59ed93ac415ba82ff50ee0a95b51
jesse <Teacher@Teachers-MacBook-Pro.local>
2016-12-31 22:17:29 +1300

    Trying to nest comments

--- src/app/milestones/milestones.component.ts
+++ src/app/milestones/milestones.component.ts
@@ -26,16 +26,50 @@ export class MilestonesComponent implements OnInit {
 
   this.milestones = af.database.list(`milestones/${this.id}`)
   	.map((milestones) => {
+      // console.log(milestones)
   		return milestones.map((milestone) => 
   		{
-  			milestone.todos = af.database.list(`/todos/${milestone.$key}`)
-  			milestone.resources = af.database.list(`/resources/${milestone.$key}`)
-        milestone.marking = af.database.list(`/marking/${milestone.$key}`)
-        milestone.stream = af.database.list(`/stream/${milestone.$key}`)
-  			return milestone
-
+        // console.log(milestone)
+  			// milestone.todos = af.database.list(`/todos/${milestone.$key}`)
+  			// milestone.resources = af.database.list(`/resources/${milestone.$key}`)
+     //    milestone.marking = af.database.list(`/marking/${milestone.$key}`)
+        milestone.stream = af.database.list(`/stream/${milestone.$key}`)       
+  		.map((comments) => {
+          console.log(comments)
+          return comments.map((comment) =>
+          {
+            // console.log(comment)
+            milestone.stream.comment = af.database.list(`/comments/${comment.$key}`)
+            return milestone.stream
+          })
+        })
+        return milestone
   		})
   	});
+
+
+  // Milestones is a list of milestones of a particular learning key
+
+  // Milestones.stream is a list of (pieces of information) for a particular milestone
+
+  // Milestones.stream.comment is a list of comments for each of these pieces of information
+
+  // Milestones [milestone,milestone,milestone]  
+
+  //this.milestone --------+---+------------
+  //                       M   M
+  //                         Map  
+
+  //   
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
   
   }
 
@@ -54,7 +88,6 @@ export class MilestonesComponent implements OnInit {
 
   newResource(resource, stream, form) {
     form.type = 'resource'
-    console.log(form)
     resource.push(form)
     stream.push(form)
     
