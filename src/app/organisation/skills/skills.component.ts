71b4838a9fb9d55de4fa30341351b2b6d133b07d
jesse <Teacher@Teachers-MacBook-Pro.local>
2017-01-23 21:23:13 +1300

    Basic Skill outline complete still need to work on appearance and CRUD

--- src/app/organisation/skills/skills.component.ts
+++ src/app/organisation/skills/skills.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Route, ActivatedRoute } from '@angular/router';
+import { OrganisationService } from '../../shared/organisation.service';
+import { FormGroup, FormBuilder } from '@angular/forms'
+import { Observable, Subscription } from 'rxjs';
+import 'rxjs/add/operator/map'
+import { SkillModel } from '../../shared/skill'
+
 
 @Component({
   selector: 'trk-skills',
@@ -6,10 +13,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
+	public skillsForOrganisation: SkillModel[];
+	public filteredSkillsForOrganisation: SkillModel[];
 
-  constructor() { }
+	private subscription: Subscription;
+	private id : string;
+	public form: FormGroup;
 
-  ngOnInit() {
-  }
+  	constructor(private ar: ActivatedRoute, public fb: FormBuilder, private os: OrganisationService) {
+  		this.subscription = ar.params.subscribe(
+  			(param: any) => this.id = param['id']
+  			);
+
+  		this.form = this.fb.group({
+  			title: '',
+  			description: '',
+  			infourl: ''
+  		});
+
+  	}
+
+  	create(form) {
+  		this.os.createSkillForOrganisation(this.id, form.value)
+  			.subscribe(
+  			  () => {
+  			    alert("Skill Created Successfully");
+  			    form.reset();
+  			  },
+  			  err => alert(`error creating Skill ${err}`)
+  			  );
+  	}
+
+  	delete(skillkey) {
+  		this.os.deleteSkillForOrganisation(this.id, skillkey)
+  			.subscribe(
+  			  () => alert('Approved Skill Deleted'),
+  			  console.error  
+  			  );
+  	}
+
+
+
+	ngOnInit() {
+		this.os.findAllSkills(this.id)
+			.subscribe(
+				skills => this.skillsForOrganisation = this.filteredSkillsForOrganisation = skills
+				)
+	}
+
+	search(search:string) {
+		this.filteredSkillsForOrganisation = this.skillsForOrganisation.filter(SkillModel => SkillModel.title.includes(search));
+	}
+
+	ngOnDestroy(){
+		this.subscription.unsubscribe();
+	}
 
 }
--- src/app/shared/skill.ts
+++ src/app/shared/skill.ts
@@ -2,16 +2,18 @@ export class SkillModel {
 	constructor(
 		public $key: string,
 		public title: string,
-		public about:string) {}
+		public description:string,
+		public infourl) {}
 
 		static fromJsonList(array): SkillModel[] {
 	        return array.map(SkillModel.fromJson);
 	    }
 
-	    static fromJson({$key, title, about}):SkillModel {
+	    static fromJson({$key, title, description, infourl}):SkillModel {
 	        return new SkillModel(
 	            $key,
 	            title,
-	            about);
+	            description,
+	            infourl);
 	    }
 }
